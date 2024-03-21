e4abe99845e950d74c60fb16250bf5da051f6c9c
Yo <yonansmillion29@example.com>
2024-03-21 22:45:09 +0300

    log and sigin page is made

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
+
   devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        // Other meta tags...
+      ],
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 };
